4e0808dc9b5bbfa3f87494db0a403160a01dbc1d
leocsas <leocristian2014@gmail.com>
2026-02-03 21:17:16 -0300

    Mudança de url do servidor

--- frontend/.js/script.js
+++ frontend/.js/script.js
@@ -88,7 +88,7 @@ const handleLogin = (event) => {
     login.style.display = "none"
     chat.style.display = "flex"
 
-    websocket = new WebSocket("ws://localhost:8080")
+    websocket = new WebSocket("wss://realtime-chat-wbwk.onrender.com")
     websocket.onmessage = processMessage
 }
 
